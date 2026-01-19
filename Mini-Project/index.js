14957159cc747df34a146c2f56c7409b01e33c2a
Nishit-2212 <nachiket224@gmail.com>
2026-01-19 11:29:48 +0530

    Image Error Resolve

--- Mini-Project/index.js
+++ Mini-Project/index.js
@@ -9,7 +9,7 @@ let file = {
             "stock": 99,
             "brand": "Essence",
             "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princdess/1.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
         },
@@ -22,7 +22,7 @@ let file = {
             "stock": 34,
             "brand": "Glamour Beauty",
             "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-withdd-mirror/1.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-with-mirror/thumbnail.webp"
         },
@@ -35,7 +35,7 @@ let file = {
             "stock": 89,
             "brand": "Velvet Touch",
             "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/powder-canistder/1.wedbp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/powder-canister/thumbnail.webp"
         },
@@ -48,7 +48,7 @@ let file = {
             "stock": 91,
             "brand": "Chic Cosmetics",
             "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/red-lipstickd/1.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/red-lipstick/thumbnail.webp"
         },
@@ -61,7 +61,7 @@ let file = {
             "stock": 79,
             "brand": "Nail Couture",
             "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/red-nail-poldish/1.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/red-nail-polish/thumbnail.webp"
         },
@@ -75,9 +75,7 @@ let file = {
             "stock": 4,
             "brand": "Calvin Klein",
             "images": [
-                "https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/1.webp",
-                "https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/2.webp",
-                "https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/3.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/thumbnail.webp"
         },
@@ -91,9 +89,7 @@ let file = {
             "stock": 58,
             "brand": "Chanel",
             "images": [
-                "https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/1.webp",
-                "https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/2.webp",
-                "https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/3.webp"
+                "https://commons.wikimedia.org/wiki/File:Image-not-found.png"
             ],
             "thumbnail": "https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/thumbnail.webp"
         }
@@ -284,7 +280,7 @@ const renderData = () => {
                     ${da.stock}
                 </td>
                 <td>
-                    <button class="addToCart"> Add </button>
+                    <button class="addToCart"> Add To Cart </button>
                     <button class="edit" id="editButton"> Edit </button>
                     <button class="delete id="deleteButton"> Delete </button>
                 </td>
@@ -311,9 +307,9 @@ const renderData = () => {
 })();
 
 const allCategory = new Set();
+let categoryLoad = () => {
 
-function categoryLoad() {
-    console.log('Inside category function');
+        console.log('Inside category function');
 
     let allProductData = productsData;
 
@@ -332,6 +328,7 @@ function categoryLoad() {
 
     }
 };
+
 
 
 category.addEventListener("click", () => {
@@ -417,7 +414,7 @@ const showData = (fields, searchValue) => {
                     ${da.stock}
                 </td>
                 <td>
-                    <button class="addToCart"> Add </button>
+                    <button class="addToCart"> Add To Cart</button>
                     <button class="edit" id="editButton"> Edit </button>
                     <button class="delete id="deleteButton"> Delete </button>
                 </td>
@@ -458,7 +455,7 @@ const showData = (fields, searchValue) => {
                     ${da.stock}
                 </td>
                 <td>
-                    <button class="addToCart"> Add </button>
+                    <button class="addToCart"> Add To Cart</button>
                     <button class="edit" id="editButton"> Edit </button>
                     <button class="delete id="deleteButton"> Delete </button>
                 </td>
@@ -572,73 +569,49 @@ function autocomplete(inp, arr) {
       a.setAttribute("id", this.id + "autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
       this.parentNode.appendChild(a);
-      /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
-          /*make the matching letters bold:*/
           b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
           b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
           b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
           b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
               inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
               closeAllLists();
           });
           a.appendChild(b);
         }
       }
   });
-  /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
       if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
         currentFocus++;
-        /*and and make the current item more visible:*/
         addActive(x);
       } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
         currentFocus--;
-        /*and and make the current item more visible:*/
         addActive(x);
       } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
         e.preventDefault();
         if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
           if (x) x[currentFocus].click();
         }
       }
   });
   function addActive(x) {
-    /*a function to classify an item as "active":*/
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
   function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
   function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
@@ -646,7 +619,6 @@ function autocomplete(inp, arr) {
       }
     }
   }
-  /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
   });
