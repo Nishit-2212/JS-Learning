908b1cb6e0546a304294e895fd80b437022138df
Nishit-2212 <nachiket224@gmail.com>
2026-03-02 18:00:35 +0530

    Started Angular

---
+++ Week-9/Counter/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+  
+    count = 1
+
+    handleIncrement() {
+      this.count++
+    }
+}
