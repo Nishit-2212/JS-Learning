3659cb79641c38c26aa91910d2092ed98b964316
Nishit-2212 <nachiket224@gmail.com>
2026-03-04 23:34:01 +0530

    forLoop and Two Way binding

---
+++ Week-9/forLoop/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,TwoWayBindingComponent ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  users = ['Anil', 'Sunil', 'Suresh', 'Ramesh', 'Mahesh'];
+
+}
